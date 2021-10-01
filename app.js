00eff92fbd8559b56dba0b4dd36c01501d1351e5
Chris Risden <christopherrisden@gmail.com>
2021-10-01 15:55:06 -0700

    progress but still not there

--- app.js
+++ app.js
@@ -37,12 +37,96 @@ const operators = document.querySelectorAll('.operator')
 const buttons = document.querySelectorAll('button')
 const digits = document.querySelectorAll('.digit');
 const display = document.querySelector('#display');
+const equals = document.querySelector('#equals');
+const ac = document.querySelector('#ac');
+let displayValue;
 display.innerHTML = '0';
-let displayValue = '0';
+
 let num1;
 let num2;
 let operator;
 
+
+const digitPress = function (e) {
+  if (displayValue === undefined) { //only for first press or after clear
+    displayValue = e.target.value;
+  } else if (displayValue) {
+    displayValue = displayValue + e.target.value
+  }
+
+/* technically i don't need this, i can save num1 on 1st operator press
+  if (num1 === undefined && operator === undefined) {
+    num1 = Number(displayValue)
+  } else {
+    num2 = Number(displayValue)
+  }*/
+  display.innerHTML = displayValue;
+}
+
+const clear = function (e) {
+  num1 = undefined;
+  num2 = undefined;
+  operator = undefined;
+  displayValue = undefined;
+  display.innerHTML = '0';
+}
+
+const operatorPress = function (e) {
+  displayValue = undefined;
+  if (num1 === undefined) {
+    num1 = Number(displayValue)
+  } else if (num1 && num2 && operator) {
+    equalPress();
+  }
+
+
+}
+
+const equalPress = function (e) {
+  const result = operate(operator, num1, num2)
+  displayValue = '' + result;
+  num2 = result;
+  display.innerHTML = displayValue;
+  operator = undefined;
+}
+
+digits.forEach(function(digit) {
+  digit.addEventListener('click', digitPress)
+})
+
+ac.addEventListener('click', clear)
+
+operators.forEach(function(operator) {
+  operator.addEventListener('click', operatorPress)
+})
+
+equals.addEventListener('click', equalPress)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const buttonPress = function (e) {
   const nums = ['1','2','3','4','5','6','7','8','9','0']
 
@@ -64,7 +148,7 @@ const buttonPress = function (e) {
       num1 = Number(displayValue);
       operator = this.value;
       displayValue = '0'
-    } else if (!num2) {
+    } else if (!num2 && !operator) {
       num2 = Number(displayValue)
       displayValue = operate(operator, num1, num2)
 
@@ -72,8 +156,8 @@ const buttonPress = function (e) {
       displayValue ='0'
       num1 = undefined;
       num2 = displayValue;
-    } else {
-
+    } else if (num1 && num2 && operator) {
+      displayValue = operate(operator, num1, num2)
     }
   } else if (this.classList[0] === "equals") {
     display.innerHTML = operate(operator, num1, num2)
@@ -84,3 +168,4 @@ const buttonPress = function (e) {
 buttons.forEach(function(button) {
   button.addEventListener('click', buttonPress)
 })
+*/
